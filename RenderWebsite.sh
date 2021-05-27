0c585ed812361cbc58bfdbf5d73127bdf533c5ff
Alexander Pacha <alexander.pacha@gmail.com>
2021-05-27 22:41:01 +0200

    Fixing website creation script for Windows again and re-creating the website

--- RenderWebsite.sh
+++ RenderWebsite.sh
@@ -5,17 +5,17 @@ rm *.html
 BibTeX2HTML/OSX_x86_64/bibtex2html -s omr-style --use-keys --no-keywords --nodoc -o OMR-Research-Key OMR-Research.bib
 BibTeX2HTML/OSX_x86_64/bibtex2html -s omr-style --use-keys --no-keywords --nodoc -d -r -o OMR-Research-Year OMR-Research.bib
 BibTeX2HTML/OSX_x86_64/bibtex2html -s omr-style --use-keys --no-keywords --nodoc -noabstract -o OMR-Research-No-Abstract OMR-Research.bib
-BibTeX2HTML/OSX_x86_64/bibtex2html -s omr-style --use-keys --no-keywords --nodoc -o OMR-Related-Research OMR-Related-Research.bib
+BibTeX2HTML/OSX_x86_64/bibtex2html -s omr-style --use-keys --no-keywords --nodoc -o OMR-Related OMR-Related-Research.bib
 BibTeX2HTML/OSX_x86_64/bibtex2html -s omr-style --use-keys --no-keywords --nodoc -o OMR-Unverified OMR-Research-Unverified.bib
 
 cat template/Header.html OMR-Research-Key.html template/Footer.html > index.html
 cat template/Header.html OMR-Research-Year.html template/Footer.html > omr-research-sorted-by-year.html
 cat template/Header.html OMR-Research-No-Abstract.html template/Footer.html > omr-research-compact.html
-cat template/Header.html OMR-Related-Research.html template/Footer.html > omr-related-research.html
+cat template/Header.html OMR-Related.html template/Footer.html > omr-related-research.html
 cat template/Header.html OMR-Unverified.html template/Footer.html > omr-research-unverified.html
 
 rm OMR-Research-Key.html
 rm OMR-Research-Year.html
 rm OMR-Research-No-Abstract.html
-rm OMR-Related-Research.html
+rm OMR-Related.html
 rm OMR-Unverified.html
